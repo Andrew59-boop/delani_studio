d9cef353629b62381bca58f44b03f54168586e58
Andrew59-boop <millerfaabi@gmail.com>
2020-02-23 12:42:56 +0300

    all added changes

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $(".A").hover(function(){
+      $(".Jimmy").slideToggle()
+    });
+    $(".B").hover(function(){
+      $(".yellow").slideToggle()
+    });
+    $(".C").hover(function(){
+      $(".Mig").slideToggle()
+    });
+    $(".D").hover(function(){
+      $(".BlackDark").slideToggle()
+    });
+    $(".E").hover(function(){
+      $(".Happy").fadeToggle()
+    });
+    $(".F").hover(function(){
+      $(".Settings").fadeToggle()
+    });
+    $(".G").hover(function(){
+      $(".Burned").fadeToggle()
+    });
+    $(".H").hover(function(){
+      $(".Giraffe").fadeToggle()
+    });
+    // $("").click(function(){
+      // $("#design,.design").fadeToggle()
+    });
+    // $(".pm,#pm").click(function(){
+      // $("#pm,.pm").fadeToggle()
+    });
+    // $(".dev,#dev").click(function(){
+      // $("#dev,.dev").fadeToggle()
+    });
+    // $("form.submit").click(function(){
+      var name=$(".name").val ()
+      v/ar email=$(".email").val()
+      var message=$(".class").val()
+      // alert("Hi"+name"your message has been recieved . Thank you")
+    });
+    });
+    
