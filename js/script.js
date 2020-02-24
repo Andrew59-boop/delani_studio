fc8b9bae33b2a30c4849eac72253cc56f24cbdd5
Andrew59-boop <millerfaabi@gmail.com>
2020-02-24 14:34:10 +0300

    all added changes

--- js/script.js
+++ js/script.js
@@ -8,5 +8,15 @@
     $("#development").toggle()
   })
   $("#img3").click(function(){
-    $("product").toggle()
+    $("#product").toggle()
   })
+  $(".submit").click(function(){
+      var name= $(".name").val()
+      var Email= $(".Email").val()
+      var message= $(".message").val()
+      if(name && Email && message){
+          alert(name + "we have received your message. Thank you for reaching out to us");
+      }else{
+          alert('check your input');
+      }
+      })
